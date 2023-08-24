7734fd6d237604b066f4cb8a48c91bc7c844410b
Meherdip <mrdhokane@gmail.com>
2023-08-24 10:17:14 +0530

    static running code

---
+++ Untitled-1.ts
@@ -0,0 +1,27 @@
+import {onInit,component} from '@angular/core ';
+import {Transform}from '@angular/core'
+
+@component({
+    selector:'upercaseStr',
+    template:'    ',
+})
+
+export class tootip implements onInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(){
+
+
+    }
+    setTimeout((ele)=>{
+     console.log("test")   
+    },2000)
+    function abc(test){
+        return function test(){
+
+        }
+    }
+}
